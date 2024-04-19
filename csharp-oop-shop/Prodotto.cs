48e495aeffdcbce1a5769bd3b7cbfd1ef4d0e95b
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-04-19 17:28:40 +0200

    esercizio completato

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -2,10 +2,57 @@
 {
     public class Prodotto
     {
-        public int codice {  get; set; }
-        public string nome { get; set; }
-        public string descrizione { get; set; }
+
+        //imposto la variabile in private
+        private int Codice;
+
+        //accedo al codice tramite una proprietà pubblica
+        public int codice 
+        {
+            get
+            {
+                return Codice;
+            }
+        }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        private decimal Prezzo;
         public decimal prezzo { get; set; }
-        public int iva { get; set; }
+        public int Iva { get; set; }
+
+        //COSTRUTTORE PER NUMERI RANDOM
+        //(viene chiamato automaticamente ogni volta che viene creata un'istanza)
+        public Prodotto()
+        {
+            Random rnd = new Random();
+            Codice = rnd.Next(1000, 3000);
+        }
+
+        public decimal PrezzoBase()
+        {
+            return prezzo;
+        }
+
+        public decimal PrezzoConIVA()
+        {
+            this.Iva = 22;
+            decimal prezzoConIVA = prezzo * ((decimal)Iva / 100);
+            decimal prezzoComplessivo = prezzo + prezzoConIVA;
+            return prezzoComplessivo;
+        }
+
+        public void NomeConCodice()
+        {
+            Console.WriteLine($"NOME {this.Nome} + CODICE: {this.Codice}");
+        }
+
+        //BONUS
+        public string CodiceConPadding()
+        {
+            //8 cifre con carattere 0 da sinistra, il padding funziona solo con stringa,
+            //perciò trasformo il codice in stringa
+            return Codice.ToString().PadLeft(8, '0');
+        }
+
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -6,7 +6,47 @@
     {
         static void Main()
         {
+
+            Prodotto nuovoProdotto = new Prodotto();
             
+            nuovoProdotto.prezzo = 50.99m;
+            nuovoProdotto.Nome = "Prodotto";
+            nuovoProdotto.Descrizione = "Descrizione del prodotto";
+
+            Console.WriteLine($"NOME: {nuovoProdotto.Nome}");
+            Console.WriteLine($"DESCRIZIONE: {nuovoProdotto.Descrizione}");
+            Console.WriteLine($"Il codice del prodotto è: {nuovoProdotto.codice}");
+            Console.WriteLine($"Prezzo base: {nuovoProdotto.PrezzoBase()} euro");
+            Console.WriteLine($"Il prezzo del prodotto compreso di Iva: {nuovoProdotto.PrezzoConIVA():F2}");
+            nuovoProdotto.NomeConCodice();
+
+            //BONUS 1
+            Console.WriteLine($"Codice con padding: {nuovoProdotto.CodiceConPadding()}");
+
+            //BONUS 2
+            Prodotto[] elencoProdotti = new Prodotto[2];
+
+            //prodotti
+            elencoProdotti[0] = new Prodotto 
+            { 
+                Nome = "Telefono",  
+                Descrizione = "Descrizione telefono", 
+                prezzo = 123.12m 
+            };
+            elencoProdotti[1] = new Prodotto
+            { 
+                Nome = "Computer",
+                Descrizione = "Descrizione computer",
+                prezzo = 1234.12m 
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("Elenco dei prodotti nel negozio:");
+            foreach (Prodotto prodotto in elencoProdotti)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"- Nome: {prodotto.Nome}, \nDescrizione: {prodotto.Descrizione}, \nCodice: {prodotto.codice},\nPrezzo base: {prodotto.PrezzoBase()}, \nPrezzo compreso di IVA al 22%: {prodotto.PrezzoConIVA():F2} euro");
+            }
         }
     }
 }
